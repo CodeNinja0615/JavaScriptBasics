fab9bac57a41e270f1dc196f9834293cce7ca09f
Andyadav1 <anandkhushi309@gmail.com>
2026-02-27 20:21:50 +0530

    inheritance

--- PracticeCode/inheritance.js
+++ PracticeCode/inheritance.js
@@ -1,11 +1,14 @@
 import Person from './basic07.js'
 
+//Inheritance Concept
 class Pet extends Person{
 
     constructor(firstName, lastName){
+        //sends tthe balue to the inherited class constructor
         super(firstName, lastName)
 
     }
+    
     get location(){
         return "Allahabad"
     }
